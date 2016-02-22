007544918f89d957ee1458cafc88536211abf7d8
Syed Salahuddin <syedamin@cs.nyu.edu>
2016-02-21 20:09:05 -0500

    bike shedding

--- W02/D01/Instructor_Notes/functions/solutions/buttons/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-  var butt = document.querySelector('button');
-  butt.addEventListener("click", function(eventObject){
-    console.log(eventObject);
-    var el = document.createElement("img");
-    el.src = "http://i.imgur.com/yurTtSn.gif";
-    document.body.appendChild(el);
-  });
-});
