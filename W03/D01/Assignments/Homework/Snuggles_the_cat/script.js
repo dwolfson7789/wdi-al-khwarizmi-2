62d35bce11ac161dda25d889be9dbb760fbfa5b7
Ethan Friedman <ethanjfriedman@gmail.com>
2016-02-24 09:59:57 -0500

    starting snuggles

--- W03/D01/Assignments/Homework/Snuggles_the_cat/script.js
+++ W03/D01/Assignments/Homework/Snuggles_the_cat/script.js
@@ -4,7 +4,27 @@ var left = 0;
 var dir = 1;
 
 //this bounces snuggles back and forth
-window.setInterval(function() {
+// window.setInterval(function() {
+//   cat.style.left = left + "px";
+//   if (dir) {
+//     if (left <= window.innerWidth - 300) {
+//       left += 10;
+//     } else {
+//       dir = 0;
+//       cat.classList.toggle('right')
+//     }
+//   } else {
+//     if (left >= 0) {
+//       left -= 10;
+//     } else {
+//       dir = 1;
+//       cat.classList.toggle('right');
+//     }
+//   }
+// }, 10);
+
+window.onload = function() {
+var foo = setInterval(function() {
   cat.style.left = left + "px";
   if (dir) {
     if (left <= window.innerWidth - 300) {
@@ -22,3 +42,5 @@ window.setInterval(function() {
     }
   }
 }, 10);
+
+}
