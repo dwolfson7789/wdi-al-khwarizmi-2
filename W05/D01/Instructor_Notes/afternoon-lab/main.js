ca7154362b851d2ba988c275305d318edb0f91a3
Syed Salahuddin <syedamin@cs.nyu.edu>
2016-03-14 13:11:27 -0400

    adding another example

--- W05/D01/Instructor_Notes/afternoon-lab/main.js
+++ W05/D01/Instructor_Notes/afternoon-lab/main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event){
     // example call
     // if you get a 504 error then that means you have an incorrect request
     // replace 1234 with your nytimes api key!
-    url: 'http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/C000880.json?api-key=1234',
+    url: 'http://api.nytimes.com/svc/politics/v3/us/legislative/congress/113/house/members/current.json?api-key=1234',
     dataType: 'jsonp',
     success: function(response){
       console.log(response);
