34960543775a953d6a578e7c766eb3c775bcedb2
Syed Salahuddin <syedamin@cs.nyu.edu>
2016-03-14 13:03:06 -0400

    removing my api key :P

--- W05/D01/Instructor_Notes/afternoon-lab/main.js
+++ W05/D01/Instructor_Notes/afternoon-lab/main.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event){
 
   $.ajax({
-    //example call
-    //if you get a 504 error then that means you have an incorrect request
-    url: 'http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/C000880.json?api-key=8e4fe03bcb43ab502d5dbb6bb5994b39:13:74703480',
+    // example call
+    // if you get a 504 error then that means you have an incorrect request
+    // replace 1234 with your nytimes api key!
+    url: 'http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/C000880.json?api-key=1234',
     dataType: 'jsonp',
     success: function(response){
       console.log(response);
