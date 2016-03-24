80089e7b4838e148e160624e7302fb973d78eee1
Syed Salahuddin <syedamin@cs.nyu.edu>
2016-03-23 20:08:40 -0400

    fixing contactr backend

--- W06/D04/Instructor_Notes/contactr/app.js
+++ W06/D04/Instructor_Notes/contactr/app.js
@@ -1,13 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var cors = require('cors');
 var fs = require('fs');
+
 var app = express();
 
-/* let's add the ability ajax to our server from anywhere! */
+/* add neat features to our server */
 app.use(cors());
+app.use(bodyParser.json());
 
 
-/* depending on the kind of request fire the right callback */
+/* depending on the kind of request, fire the right callback */
 app.get('/', function(request, response){
   fs.readFile('./contacts.json', function(err, data){
     response.json(JSON.parse(data));
@@ -15,17 +18,20 @@ app.get('/', function(request, response){
 });
 
 app.post('/', function(request, response){
-  //read the current contents, push. then write.
-  fs.readFile('./contacts.json', function(err, data){
-    var parsed  = JSON.parse(data);
-    parsed.push({ "name" : "salahuddin"});
-    fs.writeFile('./contacts.json', JSON.stringfiy(parsed), function(err){
-      response.json('{"success": "true"}')
+  //read the current contents.
+  fs.readFile('./contacts.json',  function(err, data){
+    // parse and push the body of our request on to our array
+    var parsed = JSON.parse(data);
+    // body parser gives us the body property
+    parsed.push(request.body);
+    fs.writeFile('./contacts.json', JSON.stringify(parsed), function(err){
+      // this can be anything
+      response.json('{"success":"true"}');
     });
   });
 });
 
-/* listen to requests */
+/* listen to requests from the operating system */
 app.listen(3000, function(){
-  console.log('listen to events on a "port" of the computer')
+  console.log('listen to events on a "port" (3000) of the localhost of this computer')
 });
