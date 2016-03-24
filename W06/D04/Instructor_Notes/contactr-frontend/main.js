63d3dcad30d149fcfaa48debd002bc1353c0ea52
Syed Salahuddin <syedamin@cs.nyu.edu>
2016-03-23 21:24:59 -0400

    finished be for contactr

--- W06/D04/Instructor_Notes/contactr-frontend/main.js
+++ W06/D04/Instructor_Notes/contactr-frontend/main.js
@@ -1,5 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  $.getJSON( 'http://localhost:3000/', function( json ) {
-    console.log(json);
-   });
+
 });
--- W06/D04/Instructor_Notes/contactr/app.js
+++ W06/D04/Instructor_Notes/contactr/app.js
@@ -9,14 +9,68 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
 /* depending on the kind of request, fire the right callback */
 app.get('/', function(request, response){
   fs.readFile('./contacts.json', function(err, data){
-    response.json(JSON.parse(data));
-  })
+    response.json('{"description":"contactr api", "version": "0.1.0"}');
+  });
 });
 
+/* get all contacts */
+app.get('/contacts', function(request, response){
+  fs.readFile('./contacts.json', function(err, data){
+    response.json(JSON.parse(data));
+  });
+});
+
+/* get names  */
+app.get('/names', function(request, response){
+  fs.readFile('./contacts.json', function(err, data){
+    var contacts = JSON.parse(data);
+    // reformat the array so we only have names.
+    var names = contacts.map(function(obj){
+      var name = {};
+      name["name"] = obj.name;
+      return name;
+    });
+    response.json(names);
+  });
+});
+
+/* get emails*/
+app.get('/emails', function(request, response){
+  fs.readFile('./contacts.json', function(err, data){
+    var contacts = JSON.parse(data);
+    // reformat array
+    var emails = contacts.map(function(obj){
+      var email = {};
+      email["email"] = obj.email;
+      return email
+    });
+    response.json(emails);
+  });
+});
+
+/* query for a specific contact */
+app.get('/contact', function(request, response){
+  console.log(request.query);
+  fs.readFile('./contacts.json', function(err, data){
+    var parsed = JSON.parse(data);
+    var name = parsed.filter(function(obj){
+      if(obj["name"] == request.query.name){
+        return true;
+      } else{
+        return false;
+      }
+    });
+
+    // check if the filter has any names.
+    name.length >= 1 ? response.json(name) : response.status(404).send('Not found!');
+
+  });
+});
+
+/* post */
 app.post('/', function(request, response){
   //read the current contents.
   fs.readFile('./contacts.json',  function(err, data){
