ab818498c91cfdd01a71eb57469ce528883d2623
Syed Salahuddin <syedamin@cs.nyu.edu>
2016-03-30 23:37:17 -0400

    some more tiny changes

--- W07/D04/Instructor_Notes/react102/app/App.js
+++ W07/D04/Instructor_Notes/react102/app/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
 
-const HelloWorld = React.createClass({
-  render() {
-    return (
-      <h2>Hey this is, {this.props.name}</h2>
-    );
-  }
-});
 
 const FriendsContainer = React.createClass({
-  render: function(){
-    const name = 'Syed'
-    const friends = ['Ivan', 'Joe', 'Kunal']
+  render(){
+    var name = 'Syed'
+    var friends = ['Ivan', 'Tims', 'Kaho']
     return (
       <div>
-        <h3> Name: {name} </h3>
+        <h3>Name: {name}</h3>
         <ShowList names={friends} />
       </div>
     )
   }
-})
+});
+
+
+const ShowList = React.createClass({
+  render: function(){
+    // yess
+    var listItems = this.props.names.map(function(friend){
+      return <li> { friend } </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
+const HelloWorld = React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>Hey this is, {this.props.name}, ok</h2>
+        <FriendsContainer />
+      </div>
+    );
+  }
+});
 
 // little help from my module friends
 export default HelloWorld;
