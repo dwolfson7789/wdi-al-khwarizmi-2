ca888123d5d906e13198f3900e19dc52efdf74cf
Syed Salahudin <syedamin@cs.nyu.edu>
2016-04-01 11:06:52 -0400

    hello react 102

---
+++ W07/D05/Instructor_Notes/in-class-react-syed/app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    <p>hot reloading, es2015 import, bablerc </p >
+  </div>,
+  document.getElementById('app')
+)
