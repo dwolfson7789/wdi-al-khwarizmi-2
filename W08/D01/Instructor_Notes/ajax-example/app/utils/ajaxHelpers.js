d02821d995a021128f72e9bfc5335bcd3c01a96d
Syed Salahudin <syedamin@cs.nyu.edu>
2016-04-04 13:29:45 -0400

    ajax example

--- W08/D01/Instructor_Notes/ajax-example/app/utils/ajaxHelpers.js
+++ W08/D01/Instructor_Notes/ajax-example/app/utils/ajaxHelpers.js
@@ -1,23 +1,20 @@
 import axios from 'axios';
 
-function getContacts(contact){
-  return axios.get('https://localhost:3000/contacts')
-}
-
 const helpers = {
-  getContactName: function(contacts){
+  getContactName: function(){
     // fetch some data from using a promise
-    return axios.all(players.map(function (username){
-        return getUserInfo(username);
-    })).then(function (response){ //kind of like on() from jquery
-      // when promise is resolved
-      // map or loop over the two calls so we just get data
-      return response.map(function (restaurant){
-        return restaurant.data; // this returns another promise
-      });
-    }).catch(function(err){
+    axios.get('http://localhost:3000/contacts')
+    /*
+    .then(function(response){
+      console.log(response);
+      return response;
+    })
+    .catch(function(err){
       // good practice to add to promise chains
       console.warn('Error in ');
     })
+    */
   }
 }
+
+export default helpers;
