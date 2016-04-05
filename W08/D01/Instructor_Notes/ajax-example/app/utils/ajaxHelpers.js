c4843762970eab3b6ae155390ffd5d4a6c3bc198
dwolfson7789 <Drew@Drews-MacBook-Pro.local>
2016-04-05 13:59:31 -0400

    comm

--- W08/D01/Instructor_Notes/ajax-example/app/utils/ajaxHelpers.js
+++ W08/D01/Instructor_Notes/ajax-example/app/utils/ajaxHelpers.js
@@ -1,19 +1,12 @@
 import axios from 'axios';
 
 const helpers = {
-  getContactName: function(){
+  getContacts: function(){
     // fetch some data from using a promise
-    axios.get('http://localhost:3000/contacts')
-    /*
-    .then(function(response){
-      console.log(response);
-      return response;
-    })
-    .catch(function(err){
-      // good practice to add to promise chains
-      console.warn('Error in ');
-    })
-    */
+    return axios.get('http://localhost:3000/contacts');
+  },
+  addContact: function(contact){
+    return axios.post('http://localhost:3000/contacts/new', contact);
   }
 }
 
