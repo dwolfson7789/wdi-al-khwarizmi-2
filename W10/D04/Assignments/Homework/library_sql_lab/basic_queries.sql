7bf9a38747c873c77bad92d79673e44b6b51784e
Liza Ramo <liza.h.ramo@gmail.com>
2016-04-21 14:31:50 -0400

    psql lesson

---
+++ W10/D04/Assignments/Homework/library_sql_lab/basic_queries.sql
@@ -0,0 +1,26 @@
+-- BASIC QUERIES
+-- Unless otherwise stated, all queries should return all columns
+
+-- Get all information about all authors
+SELECT * FROM authors;
+
+-- Get just the name and birth year of all authors
+
+-- Get all authors from the USA
+
+-- Get all books published in 1985
+
+-- Get just the title of all books.
+
+-- Get just the year that 'A Dance with Dragons' was published
+  -- Cry when you realize how long it's been
+
+-- Add yourself as an author
+
+-- Add two books that you'd like to write (you can hard-code your id as the author id)
+
+-- Update one of your books to have a new title
+
+-- Delete your books
+
+-- Delete your author entry
