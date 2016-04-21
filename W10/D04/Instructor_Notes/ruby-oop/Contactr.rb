6eaefa9640dd9596712ffbe9e26d8965bb675dae
Syed Salahudin <syedamin@cs.nyu.edu>
2016-04-21 09:53:09 -0400

    lesson plan

--- W10/D04/Instructor_Notes/ruby-oop/Contactr.rb
+++ W10/D04/Instructor_Notes/ruby-oop/Contactr.rb
@@ -11,13 +11,15 @@ class Contacts
   def get
     @contacts
   end
-
+  # update
   def update(old_person, new_person)
-    old_person
+    # find old object and update to new object
+    idx = @contacts.index(old_person)
+    @contacts[idx] = new_person
   end
-
+  # delete
   def delete(person)
-    person
+    @contacts.delete(person)
   end
 end
 
@@ -28,12 +30,21 @@ class Person
     @number = number
     @person = {name: @name, number: @number}
   end
-
+  # get name and number
   def get
     @person
   end
+  # set name and number
+  def set_name(name)
+    @person[:name] = name
+  end
+
+  def set_number(number)
+    @person[:number] = number
+  end
 end
 
+# this area below is to just test some stuff
 list = Contacts.new()
 
 syed = Person.new("syed salahuddin", "2011112333")
@@ -43,8 +54,28 @@ meow = Person.new("meow", "2011112333")
 list.add(syed)
 list.add(drake)
 list.add(meow)
+list.update(syed, drake)
 contacts = list.get
+
 contacts.each do |contact|
-  puts contact.inspect
+  puts contact.get
 end
-puts syed.get
+
+# command line crud!
+puts "add contact"
+puts "add name"
+name = gets
+puts "add number"
+number = gets
+list.add(Person.new(name.chomp, number.chomp))
+
+contacts.each do |contact|
+  puts contact.get
+end
+
+puts "welcome to contactr!"
+puts "enter a choice: 1. to add, 2. delete, 3. update, 4. get all contacts, 5. to end program"
+choice = gets
+while choice.chomp != "5"
+  puts "do the things"
+end
